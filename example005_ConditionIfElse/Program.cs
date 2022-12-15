5c04d26ac1e38fbdcde0bd7278685f8924b6c889
Dzuder <dzuder14@yandex.ru>
2022-12-15 21:04:33 +0500

    created welcom program

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write(" Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "амир")
+{
+    Console.WriteLine("Приветсвую, хозяин!!");
+}
+else
+{
+    Console.Write("Оу, ты не мой хозяин. Но так уж и быть, я поприветствую тебя.");
+    Console.WriteLine("Здравствуй, " + username + ", но на этом всё!!!");
+}
